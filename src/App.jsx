4b115659c19edb9c64ddb6d05592a3ffc80f59f8
Nooshin bakhtiari <nooshindev@gmail.com>
2023-10-12 12:13:08 +0330

    add SVG instead of image tag in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,13 @@ function Header() {
   return (
     <header className="header">
       <h1>TODO</h1>
-      <img src="src\assets\icon-sun.svg" alt="headder icon" role="button" />
+      <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26">
+        <path
+          fill="#FFF"
+          fill-rule="evenodd"
+          d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"
+        />
+      </svg>
     </header>
   );
 }
@@ -150,13 +156,30 @@ function ListItems({
                 />
 
                 <p className={list.isChecked ? "checked" : ""}>{list.items}</p>
-                <img
+                {/* <img
                   src="/src\assets\icon-cross.svg"
                   alt={list.id}
                   className="icon-remove"
                   role="button"
                   onClick={() => removeList(list.id)}
-                />
+                /> */}
+                <span
+                  className="icon-remove"
+                  onClick={() => removeList(list.id)}
+                  role="button"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                  >
+                    <path
+                      fill="#494C6B"
+                      fill-rule="evenodd"
+                      d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+                    />
+                  </svg>
+                </span>
               </div>
             </li>
           ))
@@ -187,7 +210,9 @@ function Footer({ itemChecked, remove, setFilter, filteredList }) {
           Active{" "}
         </button>
         <button
-          className={`btn btn-footer ${filteredList === "complete" ? "active" : ""}`}
+          className={`btn btn-footer ${
+            filteredList === "complete" ? "active" : ""
+          }`}
           onClick={() => setFilter("complete")}
         >
           Compeleted
