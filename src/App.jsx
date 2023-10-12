6c382e63bd0c6c2787acfd1c8c191d59772e2d6b
Nooshin bakhtiari <nooshindev@gmail.com>
2023-10-12 10:32:33 +0330

    filter the task completed - active - or all task

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,51 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [items, setItems] = useState("");
   const [List, setList] = useState([]);
+  const [activeItems, setActiveItems] = useState([]);
   const [activeRange, setActiveRange] = useState(0);
   const [filteredList, setFilteredList] = useState("all");
-  const [activeItems, setActiveItems] = useState([]);
-
-  const updateFilter = (newFilter) => {
-    setFilteredList(newFilter);
-  };
-  function handleFilterChange(NewFilter) {
-    if (NewFilter === "complete") {
-      updateFilter(NewFilter);
-
-      // setList(List.filter((item) => item.isChecked === true));
-      console.log(List.filter((item) => item.isChecked === true));
-       // useEffect(() =>{
-  //   setActiveItems(List.filter((item) => !item.isChecked));
-  // },[filteredList , List]);
-    } else if (NewFilter === "active") {
-      updateFilter(NewFilter);
-
-      // setList(List.filter((item) => item.isChecked === false));
-    } else {
-      updateFilter(NewFilter);
-    }
-  }
+  const [items, setItems] = useState("");
 
   function handleChange(e) {
     setItems(e.target.value);
   }
   function handleSubmit(e) {
     e.preventDefault();
-    setList([
-      ...List,
-      {
+    if (items.trim() !== "") {
+      const newItem = {
         items: items,
         id: Date.now(),
         isChecked: false,
-      },
-    ]);
-    setItems("");
+        active: true,
+      };
+      setList([...List, newItem]);
+
+      setActiveItems([...List, newItem]);
+
+      setItems("");
+    }
   }
+  useEffect(() => {
+    const filteredTasks = () => {
+      if (filteredList === "active") {
+        return List.filter((item) => !item.isChecked);
+      } else if (filteredList === "complete") {
+        return List.filter((item) => item.isChecked);
+      } else {
+        return List;
+      }
+    };
+
+    setActiveItems(filteredTasks());
+  }, [List, filteredList]);
 
   function handleToggleItems(id) {
     setList(
       List.map((item) =>
-        item.id === id ? { ...item, isChecked: !item.isChecked } : item
+        item.id === id
+          ? { ...item, isChecked: !item.isChecked, active: !item.active }
+          : item
       )
     );
   }
@@ -56,9 +54,9 @@ function App() {
   const itemtotal = List.length;
   const totalCalcuate = itemtotal - itemChecked;
 
-  // useEffect(() =>{
-  //   setActiveItems(List.filter((item) => !item.isChecked));
-  // },[filteredList , List]);
+  function removeItemesCompleted() {
+    setList(List.filter((item) => !item.isChecked));
+  }
 
   return (
     <>
@@ -73,6 +71,9 @@ function App() {
           List={List}
           items={items}
           handleToggleItems={handleToggleItems}
+          filteredList={filteredList}
+          activeItems={activeItems}
+          // filteredTasks={filteredTasks}
         >
           <Footer
             items={List}
@@ -80,7 +81,8 @@ function App() {
             activeRange={activeRange}
             setActiveRange={setActiveRange}
             List={List}
-            handleFilterChange={handleFilterChange}
+            remove={removeItemesCompleted}
+            setFilter={setFilteredList}
           />
         </ListItems>
       </Main>
@@ -118,55 +120,60 @@ function AddListInput({ items, handleChange, handleSubmit }) {
   );
 }
 
-function Footer({ itemChecked, handleFilterChange }) {
+function Footer({ itemChecked, remove, setFilter }) {
   return (
     <div className="footer">
       <span className="item-left">{itemChecked} item Left</span>
       <div className="button-filter">
-        <button
-          className="btn btn-footer"
-          onClick={() => handleFilterChange("all")}
-        >
+        <button className="btn btn-footer" onClick={() => setFilter("all")}>
           All
         </button>
-        <button
-          className="btn btn-footer"
-          onClick={() => handleFilterChange("active")}
-        >
+        <button className="btn btn-footer" onClick={() => setFilter("active")}>
           Active{" "}
         </button>
         <button
           className="btn btn-footer"
-          onClick={() => handleFilterChange("complete")}
+          onClick={() => setFilter("complete")}
         >
           Compeleted
         </button>
       </div>
-      <button className="btn btn-clear">Clear Completed</button>
+      <button className="btn btn-clear" onClick={remove}>
+        Clear Completed
+      </button>
     </div>
   );
 }
 
-function ListItems({ children, List, handleToggleItems }) {
+function ListItems({ children, handleToggleItems, filteredList, activeItems }) {
   return (
     <>
       <ul className="Lists">
-        {List.length === 0 ? (
-          <p className="NotTodo"> Not to do yet </p>
+        {activeItems.length === 0 ? (
+          filteredList === "active" ? (
+            <p className="NotTodo ">No active todo</p>
+          ) : filteredList === "complete" ? (
+            <p className="NotTodo ">No completed todo</p>
+          ) : (
+            <p className="NotTodo ">No todo yet </p>
+          )
         ) : (
-          List.map((list, i) => (
+          activeItems.map((list, i) => (
             <li className="listItems" key={i}>
               <div className="inputs" id={i}>
                 <input
                   type="checkbox"
                   onChange={() => handleToggleItems(list.id)}
+                  checked={list.isChecked}
                 />
+                 <img src="src\assets\" alt="" />
                 <p className={list.isChecked ? "checked" : ""}>{list.items}</p>
               </div>
             </li>
           ))
         )}
       </ul>
+
       {children}
     </>
   );
